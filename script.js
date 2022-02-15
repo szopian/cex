b3ddfbb5b3f70d9bb8b8e02396a80b9f650f71a4
Greg Szopian <gsz@Gregs-MacBook-Pro.local>
2022-02-15 13:34:57 +0000

    var, let , const

---
+++ script.js
@@ -0,0 +1,15 @@
+
+let fname;
+fname = 'Greg';
+
+function printName(){
+
+console.log('Current character is ' + fname + ' ' + f2name);
+}
+
+printName();
+
+var f2name;
+f2name= 'Not Greg';
+
+let 
