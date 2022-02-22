1944b804c886a9557a987d796296da149ce2b44d
szopian <szopiangreg@gmail.com>
2022-02-22 14:47:24 +0000

    add typecasting and object

--- script.js
+++ script.js
@@ -1,3 +1,70 @@
+// ================++++>>>---ARRAY---<<<+=++++=======
+
+// ========.>>> OBJECT =====++++++
+
+/*
+
+function Animal() {
+  this.type = "dog";
+  this.sound = function () {
+    console.log("woof");
+  };
+}
+
+let dog = new Animal();
+dog.sound();
+
+// Object literal
+
+let animal = {
+  type: "dog",
+  sound: function () {
+    console.log("woof");
+  },
+};
+
+animal.sound();
+
+/*
+
+//=======>>>TYPECASTING<<=============
+
+//  parseFloat() parse argument returning a floating point NUMBER..
+let q = parseFloat(3.14);
+let w = parseFloat("3.14");
+let e = parseFloat("  3.14  ");
+let r = parseFloat("314e-2");
+let t = parseFloat("0.0314E+2");
+let y = parseFloat("3.14some non-digit characters");
+let u = parseFloat({
+  toString: function () {
+    return "3.14";
+  },
+});
+
+console.log(
+  q + " | " + w + " | " + e + " | " + r + " | " + t + " | " + y + " | " + u
+);
+
+//  toString() converting number to string!!
+let n = 7;
+console.log(typeof n);
+n = toString(n);
+console.log(typeof n);
+n = parseInt(n);
+console.log(typeof n);
+
+//  Number()
+let num = "6";
+console.log(typeof num);
+num = Number(num);
+console.log(typeof num);
+
+//METHOD example..
+let amount = 5123.1234;
+console.log(amount.toFixed(2));
+
+
 // HOISTING
 
 let nameLet = "BooLet";
@@ -11,7 +78,6 @@ var nameVar = "BooVar";
 printName();
 
 // LOCAL Variable example..
-/*
 
 var name = "Bob";
 
